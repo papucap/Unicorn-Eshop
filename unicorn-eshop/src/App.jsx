dce86a06c92e95016431dd106accbdac0bd2fbd0
komverca <kom.verca@gmail.com>
2026-05-25 12:30:04 +0200

    ProductInfo-css

--- unicorn-eshop/src/App.jsx
+++ unicorn-eshop/src/App.jsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
 import "./App.css";
-import ProductInfo from "./components/ProductInfo/ProductInfo";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>
-    <ProductInfo></ProductInfo>
-  </>;
+  return <></>;
 }
 
 export default App;
