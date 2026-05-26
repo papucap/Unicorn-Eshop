8019cd2463c5d69a1224876fcb1ab627f597eba6
papucap <papucapatrik@gmail.com>
2026-05-26 10:35:26 +0200

    Routes in App.jsx

--- unicorn-eshop/src/App.jsx
+++ unicorn-eshop/src/App.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import NavBar from './components/NavBar/NavBar';
 import { getAllProducts } from './api/api';
 import Products from './pages/Products/Products';
+import MainPage from './pages/MainPage/MainPage';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -18,7 +20,13 @@ function App() {
 
   return (
     <div>
-      <Products />
+      <NavBar onSearch={setQuery} />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/products" element={<Products products={filtered} />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
