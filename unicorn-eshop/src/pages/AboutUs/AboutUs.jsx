439a4b17a8f38f3d013f60800204a8b5d7d73a6f
papucap <papucapatrik@gmail.com>
2026-05-26 13:40:46 +0200

    About us added navbar header and footer

--- unicorn-eshop/src/pages/AboutUs/AboutUs.jsx
+++ unicorn-eshop/src/pages/AboutUs/AboutUs.jsx
@@ -1,7 +1,16 @@
-export default function AboutUs () {
-    return (
-        <div>
-            <h1>O NAS PAGE</h1>
-        </div>
-    )
+import NavBar from "../../components/NavBar/NavBar";
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+
+export default function AboutUs() {
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <div>
+        <h1>O NAS PAGE</h1>
+      </div>
+      <Footer />
+    </>
+  );
 }
