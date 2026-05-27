e9e8a311ebfc01e605123b5513e6bece5c0a904d
Ra1chl <raichljonas1@gmail.com>
2026-05-27 10:39:50 +0200

    Product Box uprava

--- unicorn-eshop/src/components/MainPage/ProductBox.jsx
+++ unicorn-eshop/src/components/MainPage/ProductBox.jsx
@@ -1,87 +1,39 @@
 import "./ProductBox.css";
 
-const PRODUCTS_DATA = [
-  {
-    id: 1,
-    brand: "NIKE",
-    name: "AirMax 3",
-    price: 3555,
-    images: [
-      "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_t.png",
-    ],
-  },
-  {
-    id: 2,
-    brand: "NIKE",
-    name: "AirMax 3",
-    price: 3555,
-    images: [
-      "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_t.png",
-    ],
-  },
-  {
-    id: 3,
-    brand: "NIKE",
-    name: "AirMax 3",
-    price: 3555,
-    images: [
-      "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_t.png",
-    ],
-  },
-  {
-    id: 4,
-    brand: "NIKE",
-    name: "AirMax 3",
-    price: 3555,
-    images: [
-      "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_t.png",
-    ],
-  },
-  {
-    id: 5,
-    brand: "NIKE",
-    name: "AirMax 3",
-    price: 3555,
-    images: [
-      "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_t.png",
-    ],
-  },
-];
-
-function ProductBox({ title = "Produkty" }) {
+function ProductBox({ title = "Produkty", products = [] }) {
   return (
-    <>
-      <div className="productSection">
-        <div className="productSection-top">
-          <h1>{title}</h1>
+    <div className="productSection">
+      <div className="productSection-top">
+        <h1>{title}</h1>
 
-          <button
-            className="show-all-btn"
-            onClick={() => (window.location.href = "/products")}
-          >
-            Zobrazit vše
-          </button>
-        </div>
-
-        <div className="productContainer">
-          {PRODUCTS_DATA.map((product) => (
-            <div className="product" key={product.id}>
-              <div className="images-product">
-                {product.images.map((img, i) => (
-                  <img key={i} src={img} alt={product.name} className="img" />
-                ))}
-              </div>
-
-              <div className="info-product">
-                <h1 className="brand-product">{product.brand}</h1>
-                <h1 className="name-product">{product.name}</h1> <br />
-                <p>{product.price.toLocaleString()} Kč</p>
-              </div>
-            </div>
-          ))}
-        </div>
+        <button
+          className="show-all-btn"
+          onClick={() => (window.location.href = "/products")}
+        >
+          Zobrazit vše
+        </button>
       </div>
-    </>
+
+      <div className="productContainer">
+        {products.map((product) => (
+          <div className="product" key={product.id}>
+            <div className="images-product">
+              {product.images.map((img, i) => (
+                <img key={i} src={img} alt={product.name} className="img" />
+              ))}
+            </div>
+
+            <div className="info-product">
+              <h1 className="brand-product">{product.brand}</h1>
+
+              <h1 className="name-product">{product.name}</h1>
+
+              <p>{product.price.toLocaleString()} Kč</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
