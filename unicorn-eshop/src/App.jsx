e582590ad94621c6471ab15d950d46551b9fb88a
Ra1chl <raichljonas1@gmail.com>
2026-05-27 11:55:21 +0200

    Main page update ( const lang)

--- unicorn-eshop/src/App.jsx
+++ unicorn-eshop/src/App.jsx
@@ -1,24 +1,42 @@
-import { Routes, Route } from 'react-router-dom';
-import Products from './pages/Products/Products';
-import MainPage from './pages/MainPage/MainPage';
-import AboutUs from './pages/AboutUs/AboutUs';
-import ContactPage from './pages/Contact/Contactpage';
-import Cart from './pages/Cart/Cart'
-import "./App.css"
-import QRPage from './pages/Cart/QR/QRPage';
+import { useState } from "react"; // Přidán import pro useState
+import { Routes, Route } from "react-router-dom";
+import Products from "./pages/Products/Products";
+import MainPage from "./pages/MainPage/MainPage";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import ContactPage from "./pages/Contact/Contactpage";
+import Cart from "./pages/Cart/Cart";
+import "./App.css";
+import QRPage from "./pages/Cart/QR/QRPage";
 
 function App() {
+  // Hlavní stav pro jazyk celého eshopu
+  const [lang, setLang] = useState("cs");
+
   return (
     <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/products" element={<Products />} />
-      <Route path="/products/:category" element={<Products />} />
-      <Route path = "/about" element = {<AboutUs />} />
-      <Route path = "/contact" element ={<ContactPage />} />
-      <Route path = "/cart" element = {<Cart />} />
-      <Route path = "/cart/QR" element = {<QRPage/>} />
+      <Route path="/" element={<MainPage lang={lang} setLang={setLang} />} />
+      <Route
+        path="/products"
+        element={<Products lang={lang} setLang={setLang} />}
+      />
+      <Route
+        path="/products/:category"
+        element={<Products lang={lang} setLang={setLang} />}
+      />
+      <Route
+        path="/about"
+        element={<AboutUs lang={lang} setLang={setLang} />}
+      />
+      <Route
+        path="/contact"
+        element={<ContactPage lang={lang} setLang={setLang} />}
+      />
+      <Route path="/cart" element={<Cart lang={lang} setLang={setLang} />} />
+      <Route
+        path="/cart/QR"
+        element={<QRPage lang={lang} setLang={setLang} />}
+      />
     </Routes>
-    
   );
 }
 
