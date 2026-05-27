7052956aa69d8e26c981f5b8d69c5422d47003f4
Ra1chl <raichljonas1@gmail.com>
2026-05-27 13:48:24 +0200

    Page update [About Us]

--- unicorn-eshop/src/components/AboutUs/AboutUs.jsx
+++ unicorn-eshop/src/components/AboutUs/AboutUs.jsx
@@ -4,7 +4,30 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import XIcon from "@mui/icons-material/X";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 
-function AboutUs() {
+function AboutUs({ lang = "cs" }) {
+  const translations = {
+    socialTitle: {
+      cs: "TISÍCE SPOKOJENÝCH ZÁKAZNÍKŮ",
+      en: "THOUSANDS OF SATISFIED CUSTOMERS",
+    },
+    socialsText: {
+      cs: "PŘESVĚDČTE SE SAMI NA NAŠICH SOCIÁLNÍCH SÍTÍCH",
+      en: "SEE FOR YOURSELF ON OUR SOCIAL MEDIA",
+    },
+    shopTitle: {
+      cs: "KAMENNÁ POBOČKA V PRAZE",
+      en: "OUR BRICK-AND-MORTAR STORE IN PRAGUE",
+    },
+    shopText: {
+      cs: "NA PŘÍKOPĚ, 110 00",
+      en: "NA PRIKOPE, 110 00",
+    },
+    shopAlt: {
+      cs: "Fotka obchodu",
+      en: "Shop photo",
+    },
+  };
+
   return (
     <>
       <div className="logo-section">
@@ -12,8 +35,8 @@ function AboutUs() {
       </div>
 
       <div className="socialsContainer">
-        <h1 id="socialTitle">TISÍCE SPOKOJENÝCH ZÁKAZNÍKŮ</h1>
-        <h3 id="socialsText">PŘESVĚDČTE SE SAMI NA NAŠICH SOCIÁLNÍCH SÍTÍCH</h3>
+        <h1 id="socialTitle">{translations.socialTitle[lang]}</h1>
+        <h3 id="socialsText">{translations.socialsText[lang]}</h3>
         <div className="socialsIcons">
           <ul className="socialsLinks">
             <li>
@@ -53,12 +76,21 @@ function AboutUs() {
       </div>
 
       <div className="shopPhotosContainer">
-        <h1 id="shopTitle">KAMENNÁ POBOČKA V PRAZE</h1>
-        <p id="shopText">NA PŘÍKOPĚ, 110 00</p>
+        <h1 id="shopTitle">{translations.shopTitle[lang]}</h1>
+        <p id="shopText">{translations.shopText[lang]}</p>
         <div className="shopPhotos">
-          <img src="./src/assets/ShopPhotos/shop1.png" alt="shopPhoto1"></img>
-          <img src="./src/assets/ShopPhotos/shop2.png" alt="shopPhoto2"></img>
-          <img src="./src/assets/ShopPhotos/shop3.png" alt="shopPhoto3"></img>
+          <img
+            src="./src/assets/ShopPhotos/shop1.png"
+            alt={`${translations.shopAlt[lang]} 1`}
+          ></img>
+          <img
+            src="./src/assets/ShopPhotos/shop2.png"
+            alt={`${translations.shopAlt[lang]} 2`}
+          ></img>
+          <img
+            src="./src/assets/ShopPhotos/shop3.png"
+            alt={`${translations.shopAlt[lang]} 3`}
+          ></img>
         </div>
       </div>
     </>
