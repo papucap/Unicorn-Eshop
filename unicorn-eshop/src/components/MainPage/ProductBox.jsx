d9acdc1160b92c081cf5e850bd0c55c570a04527
Ra1chl <raichljonas1@gmail.com>
2026-05-27 10:43:23 +0200

    Product box uprava #2

--- unicorn-eshop/src/components/MainPage/ProductBox.jsx
+++ unicorn-eshop/src/components/MainPage/ProductBox.jsx
@@ -1,39 +1,41 @@
 import "./ProductBox.css";
 
+// Přidali jsme 'products = []' jako prop
 function ProductBox({ title = "Produkty", products = [] }) {
   return (
-    <div className="productSection">
-      <div className="productSection-top">
-        <h1>{title}</h1>
+    <>
+      <div className="productSection">
+        <div className="productSection-top">
+          <h1>{title}</h1>
 
-        <button
-          className="show-all-btn"
-          onClick={() => (window.location.href = "/products")}
-        >
-          Zobrazit vše
-        </button>
-      </div>
+          <button
+            className="show-all-btn"
+            onClick={() => (window.location.href = "/products")}
+          >
+            Zobrazit vše
+          </button>
+        </div>
 
-      <div className="productContainer">
-        {products.map((product) => (
-          <div className="product" key={product.id}>
-            <div className="images-product">
-              {product.images.map((img, i) => (
-                <img key={i} src={img} alt={product.name} className="img" />
-              ))}
+        <div className="productContainer">
+          {/* Teď mapujeme produkty, které přišly přes props */}
+          {products.map((product) => (
+            <div className="product" key={product.id}>
+              <div className="images-product">
+                {product.images.map((img, i) => (
+                  <img key={i} src={img} alt={product.name} className="img" />
+                ))}
+              </div>
+
+              <div className="info-product">
+                <h1 className="brand-product">{product.brand}</h1>
+                <h1 className="name-product">{product.name}</h1> <br />
+                <p>{product.price.toLocaleString()} Kč</p>
+              </div>
             </div>
-
-            <div className="info-product">
-              <h1 className="brand-product">{product.brand}</h1>
-
-              <h1 className="name-product">{product.name}</h1>
-
-              <p>{product.price.toLocaleString()} Kč</p>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
